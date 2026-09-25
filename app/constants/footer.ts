93cc767528f4f4a4b5d828797396db678d41e0c9
Ashraf <250116877+ashrafjr-n@users.noreply.github.com>
2026-09-25 15:22:15 +0400

    make it mine V1.3

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -5,30 +5,18 @@ export const FOOTER_LINKS: FooterLink[] = [
     name: 'LinkedIn',
     hoverText: 'Connect with me',
     icon: 'icons/linkedin.svg',
-    url: 'https://www.linkedin.com/in/mohit-virli-4780b6112/',
+    url: 'https://www.linkedin.com/in/ashraf-al-jarabeah-a94509408/',
   },
   {
     name: 'GitHub',
     hoverText: 'Open Sourcing',
     icon: 'icons/github.svg',
-    url: 'https://github.com/mohitvirli',
+    url: 'https://github.com/ashrafjr-n',
   },
   {
-    name: 'Spotify',
-    hoverText: 'Curated playlists',
-    icon: 'icons/spotify.svg',
-    url: 'https://open.spotify.com/user/21hr4w2hzp4ceidewwsb4bxoy',
+    name: 'Email',
+    hoverText: 'aannaelj@gmail.com',
+    icon: 'icons/email.svg',
+    url: 'mailto:aannaelj@gmail.com',
   },
-  {
-    name: 'Instagram',
-    hoverText: '@clevirli',
-    icon: 'icons/instagram.svg',
-    url: 'https://www.instagram.com/clevirli/',
-  },
-  {
-    name: 'Resume',
-    hoverText: 'Download',
-    icon: 'icons/file.svg',
-    url: './Mohit Virli - Resume.pdf',
-  }
 ];
